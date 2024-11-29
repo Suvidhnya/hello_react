d6c3b55ecc8c61695f6c38f7e228d4a83e76bb6c
Suvidhnya <suvidnyaakdondalkar@gmail.com>
2024-11-29 21:49:48 +0530

    1st push to git

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", { id: "suvidhnya" }, "hello React");
 console.log(heading); //returns obj
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading); //converts obj to H1 tag and put it up
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+const parent = React.createElement("div",{ id: "parent" },
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", { id: "h1" }, "hi iam Suvidon"),
-    React.createElement("h1", { id: "h1" }, "hi iam Suvidon"),
+    React.createElement("h1", { id: "h2" }, "hi iam suviiii"),
   ])
 ); //returns obj
 
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,24 @@
+iport;
+const heading = React.createElement("h1", {
+    id: "suvidhnya"
+}, "hello React");
+console.log(heading); //returns obj
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading); //converts obj to H1 tag and put it up
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("div", {
+    id: "child"
+}, [
+    React.createElement("h1", {
+        id: "h1"
+    }, "hi iam Suvidon"),
+    React.createElement("h1", {
+        id: "h1"
+    }, "hi iam Suvidon")
+])); //returns obj
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+root2.render(parent);
+console.log(parent);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
